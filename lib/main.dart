f3fac5b7e7ba1422320637617810cec7256bb4e8
Guava <eugine940218@gmail.com>
2026-04-24 17:13:54 +0800

    feat: add handleNfcUri to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,57 @@ void main() {
   );
 }
 
-class EcoSensingApp extends StatelessWidget {
+class EcoSensingApp extends ConsumerStatefulWidget {
   const EcoSensingApp({super.key});
 
-  // final AppLinks _appLinks = AppLinks();
-  // StreamSubscription? _linkSubscription;
-  // String _nfcMessage = '';
+  @override
+  ConsumerState<EcoSensingApp> createState() => _EcoSensingAppState();
+}
+
+class _EcoSensingAppState extends ConsumerState<EcoSensingApp> {
+  final AppLinks _appLinks = AppLinks();
+  StreamSubscription? _linkSubscription;
+  String _nfcMessage = '尚未掃描 NFC';
+
+  @override
+  void initState() {
+    super.initState();
+    _initDeepLinks();
+  }
+
+  // 電梯搭乘追蹤 NFC 模組，接收 NFC 訊號
+  void _initDeepLinks() {
+    // 1. 處理 App 處於關閉狀態，因為掃描 NFC 而被冷啟動 (Cold Start)
+    _appLinks.getInitialLink().then((Uri? uri) {
+      if (uri != null) {
+        _handleNfcUri(uri);
+      }
+    });
+
+    // 2. 處理 App 已經在背景運行，因為掃描 NFC 而被喚醒
+    _linkSubscription = _appLinks.uriLinkStream.listen((Uri? uri) {
+      if (uri != null) {
+        _handleNfcUri(uri);
+      }
+    });
+  }
+
+  void _handleNfcUri(Uri uri) {
+    // uri 會長這樣: https://yourdomain.com/app?counter=5
+    // 我們可以把 counter 提取出來
+    final counterValue = uri.queryParameters['counter'];
+    
+    if (counterValue != null) {
+      setState(() {
+        _nfcMessage = "成功接收 NFC 資料！\nCounter: $counterValue";
+      });
+    }  }
+
+  @override
+  void dispose() {
+    _linkSubscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
